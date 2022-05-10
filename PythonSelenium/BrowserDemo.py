48f75909dccf171db077c46b50b42c1b6c0b835c
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-05-10 12:54:17 +0530

    new

--- PythonSelenium/BrowserDemo.py
+++ PythonSelenium/BrowserDemo.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path="C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:\\Users\\Shubham\\Downloads\\chromedriver.exe")
 
 driver.get("https://www.google.com")  # launch the url
 driver.maximize_window()
 print(driver.current_url)
 driver.back()  # navigate back
 driver.refresh()  # refresh
-driver.quit()
+
 
 
 
